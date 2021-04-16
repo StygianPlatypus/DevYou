4703da811107a3e56c56b00e2cb573ad59cf8fef
StygianPlatypus <82330771+StygianPlatypus@users.noreply.github.com>
2021-04-16 14:21:03 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mex = "Mexico";
+            string jam = "Jamaica";
+            Console.WriteLine("Welcome to the Vacation Budget Planner XL!\r\nWhat is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Would you like to travel to {mex} or {jam}?");
+           
+            Console.ReadLine();
+        }
+    }
+}
